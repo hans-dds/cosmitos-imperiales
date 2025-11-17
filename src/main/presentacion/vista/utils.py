b5c76fc4ef15250d360d8f0548f634945cf947ad
Herzon-Palma <herzon9@gmail.com>
2025-11-17 14:44:33 -0600

    Clean branch

--- src/main/presentacion/vista/utils.py
+++
@@ -1,5 +0,0 @@
-import plotly.express as px
-
-categorias = ['promotor', 'neutro', 'detractor']
-colores = px.colors.sequential.Viridis[len(categorias)]
-color_discrete_map = dict(zip(categorias, colores))
